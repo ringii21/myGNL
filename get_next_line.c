7852b19acea442e60b3e2d1c64c1cbdf319f9418
Anais BONARD <abonard@e3r4p5.42.fr>
2022-01-20 00:07:28 +0100

    Testeur checked, OK

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: abonard <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 15:10:22 by abonard           #+#    #+#             */
-/*   Updated: 2022/01/19 20:48:33 by abonard          ###   ########.fr       */
+/*   Updated: 2022/01/20 00:06:00 by abonard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ char	*concat(char *str, char *buffer)
 	return (new_str);
 }
 
-char	*ft_input(char *str, char *line /*int stop*/)
+char	*ft_input(char *str, char *line)
 {
 	int	i;
 	int	j;
@@ -48,7 +48,6 @@ char	*ft_input(char *str, char *line /*int stop*/)
 		return (NULL);
 	}
 	i = ft_check(str, '\n');
-//	printf("i->%d\nstop%d\n", i, stop);
 	if (i == -1)
 		i = strlen_custom(str);
 	line = malloc(sizeof(char) * i + 1);
@@ -56,8 +55,6 @@ char	*ft_input(char *str, char *line /*int stop*/)
 		return (NULL);
 	while (j < i)
 	{
-	//	if (str[george] == '\n' && stop == 1)
-	//		break;
 		line[j] = str[j];
 		j++;
 	}
@@ -70,29 +67,22 @@ char	*realloc_thing(char *str)
 	char	*new_str;
 	int		lenght_nstr;
 	int		lenght_str;
-	int		i = 0;
+	int		i = -1;
 	int		temp;
 
 	if (str[0] == '\0')
-	{
-		free(str);
-		return (NULL);
-	}
+		return (ft_frifri(str, NULL));
 	lenght_str = strlen_custom(str);
 	lenght_nstr = ft_check(str, '\n');
-//	printf("|%d|\n", lenght_nstr);
 	temp = lenght_nstr;
 	lenght_nstr = lenght_str - lenght_nstr;
 	new_str = malloc(sizeof(char) * lenght_nstr + 1);
 	if (new_str == NULL)
-	{
-		free(str);
 		return (NULL);
-	}
-	while (i < lenght_nstr)
+	if (temp != -1)
 	{
-		new_str[i] = str[temp + i];
-		i++;
+		while (++i < lenght_nstr)
+			new_str[i] = str[temp + i];
 	}
 	new_str[i] = '\0';
 	free(str);
@@ -104,10 +94,8 @@ char	*get_next_line(int fd)
 	static char	*str;
 	char		*line;
 	char		buff[BUFFER_SIZE + 1];
-//	int			stop;
 	int			i;
 
-//	stop = -1;
 	line = NULL;
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, buff, 0) < 0)
 		return (NULL);
@@ -119,11 +107,7 @@ char	*get_next_line(int fd)
 		if (str == NULL)
 			return (ft_frifri(str, NULL));
 		if ((ft_check(str, '\n') != -1) || i == 0)
-		{/*
-			if (i == 0)
-				stop = 1;*/
 			break ;
-		}
 	}
 	line = ft_input(str, line);
 	if (line == NULL)
@@ -152,6 +136,6 @@ int main(int ac, char **av)
 	//while (1)
 	//	;
 //	exit(1);
-	system("leaks a.out");
+//	system("leaks a.out");
 	return (0);
 }
